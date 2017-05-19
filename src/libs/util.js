e281feb27d659b1520fbcc64026023d6741c8cc7
wiliam qiu <weiyang.qiu@ctauto.cn>
2017-05-19 10:18:22 +0800

    update dev config

--- src/libs/util.js
+++ src/libs/util.js
@@ -8,8 +8,9 @@ const _HOST = window.location.origin;
 let ajaxUrl = _HOST+'/service';
 // 测试环境
 if(_HOST.indexOf('www.aylsonclub.com') === -1){
-	ajaxUrl='http://test.aylsonclub.com/admin'
+	//ajaxUrl='http://test.aylsonclub.com/admin'
 	//ajaxUrl='http://192.168.108:8080/admin'
+	ajaxUrl='http://localhost:6688/admin'
 	//本地后台接口
 }
 util.env=env;
